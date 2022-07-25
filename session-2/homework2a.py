d2603ee0df997a2d48003086bcd16421de4ad620
Julia Groza <jgroza24@gmail.com>
2022-07-25 16:33:57 -0400

    finalized 2a

--- session-2/homework2a.py
+++ session-2/homework2a.py
@@ -1,15 +1,29 @@
-def subarray_sum(nums):
-    sumSet = set()
-    sum = 0
+# def subarray_sum(nums):
+#     sumSet = set()
+#     sum = 0
     
-    for i in range(len(nums)):
-        sum += nums[i]
-        sumSet.add(sum)
+#     for i in range(len(nums)):
+#         sum += nums[i]
+#         sumSet.add(sum)
 
-    if(len(sumSet) < len(nums) or 0 in sumSet):
-        return True
+#     if(len(sumSet) < len(nums) or 0 in sumSet):
+#         return True
 
+#     return False
+
+# nums = [-1, -2, 3, 4, 5]
+# print(subarray_sum(nums))
+
+# option 2
+
+def subarray_zero_sum(nums):
+
+    for x in range(1, len(nums)):
+        if ((nums[x] > 0 and nums[x-1] < 0) or (nums[x] < 0 and nums[x-1] > 0)):
+            nums[x] += nums[x-1]
+        if nums[x] == 0:
+            return True
     return False
-
-nums = [-1, -2, 3, 4, 5]
-print(subarray_sum(nums))
+            
+nums = [-3, 2, 3, 1, 6]
+print(subarray_zero_sum(nums))
